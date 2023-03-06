c1aee46cc3140e7059709e38e3d551f6f30af8cb
Hrivaux <hugorivaux@gmail.com>
2023-03-06 17:09:14 +0100

    initial commit

---
+++ app/src/main/java/fr/delcey/formulairedeconnexion/AccueilActivity.java
@@ -0,0 +1,33 @@
+package fr.delcey.formulairedeconnexion;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import fr.delcey.formulairedeconnexion.R;
+
+public class AccueilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+
+
+        TextView TextView = findViewById(R.id.TextView);
+        String username = getIntent().getStringExtra("username");
+        TextView.setText("Bienvenue, " + username + " !");
+
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AccueilActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
