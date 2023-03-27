97e5c25f9d0cf0c652d9b2c56e826139637f20ef
Hrivaux <hugorivaux@gmail.com>
2023-03-27 10:46:32 +0200

    Inscription plus connexion bdd + Redac CR

--- app/src/main/java/fr/delcey/formulairedeconnexion/InscriptionUsers.java
+++ app/src/main/java/fr/delcey/formulairedeconnexion/InscriptionUsers.java
@@ -4,23 +4,30 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
-public class InscriptionUsers extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class InscriptionUsers extends AppCompatActivity {
     private EditText etNomUtilisateur;
     private EditText etMotDePasse;
     private EditText etVille;
-    private EditText etDepartement;
-    private DatePicker datePicker;
-    private RadioGroup radioGroup;
-    private RadioButton radioFemme;
-    private RadioButton radioHomme;
+    private EditText etPrenomUtilisateur;
+    private EditText etEmailUtilisateur;
+    private EditText etAdresseUtilisateur;
+    private EditText etCodepostal;
+
     private Button btnInscription;
+    private RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +35,56 @@ public class InscriptionUsers extends AppCompatActivity {
         setContentView(R.layout.activity_inscription_users);
 
         etNomUtilisateur = findViewById(R.id.et_nom_utilisateur);
+        etPrenomUtilisateur = findViewById(R.id.et_prenom_utilisateur);
+        etEmailUtilisateur = findViewById(R.id.et_email_utilisateur);
         etMotDePasse = findViewById(R.id.et_mot_de_passe);
+        etAdresseUtilisateur = findViewById(R.id.et_adresse_utilisateur);
         etVille = findViewById(R.id.et_ville);
-        etDepartement = findViewById(R.id.et_departement);
-        datePicker = findViewById(R.id.date_picker);
-        radioGroup = findViewById(R.id.radio_group);
-        radioFemme = findViewById(R.id.radio_femme);
-        radioHomme = findViewById(R.id.radio_homme);
+        etCodepostal = findViewById(R.id.et_code_postal);
         btnInscription = findViewById(R.id.btn_inscription);
+
+        mRequestQueue = Volley.newRequestQueue(this);
 
         btnInscription.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String nomUtilisateur = etNomUtilisateur.getText().toString();
+                String prenomUtilisateur = etPrenomUtilisateur.getText().toString();
+                String emailUtilisateur = etEmailUtilisateur.getText().toString();
                 String motDePasse = etMotDePasse.getText().toString();
+                String adresseUtilisateur = etAdresseUtilisateur.getText().toString();
                 String ville = etVille.getText().toString();
-                String departement = etDepartement.getText().toString();
-                int jour = datePicker.getDayOfMonth();
-                int mois = datePicker.getMonth() + 1;
-                int annee = datePicker.getYear();
-                String dateNaissance = jour + "/" + mois + "/" + annee;
-                String sexe = radioFemme.isChecked() ? "Femme" : "Homme";
+                String code_postal = etCodepostal.getText().toString();
 
-                // Faire quelque chose avec les données d'inscription ici
+                String url = "https://hugo-rivaux.fr/API/inscription.php";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(InscriptionUsers.this, "Inscription réussie!", Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(InscriptionUsers.this, "Erreur lors de l'inscription", Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("nom_utilisateur", nomUtilisateur);
+                        params.put("prenom_utilisateur", prenomUtilisateur);
+                        params.put("email_utilisateur", emailUtilisateur);
+                        params.put("mot_de_passe", motDePasse);
+                        params.put("adresse_utilisateur", adresseUtilisateur);
+                        params.put("ville", ville);
+                        params.put("code_postal", code_postal);
+                        return params;
+                    }
+                };
+                mRequestQueue.add(stringRequest);
             }
         });
     }
 }
-
--- app/src/main/java/fr/delcey/formulairedeconnexion/MainActivity.java
+++ app/src/main/java/fr/delcey/formulairedeconnexion/MainActivity.java
@@ -1,139 +1,105 @@
 package fr.delcey.formulairedeconnexion;
 
-import android.app.AlertDialog;
+
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
-import fr.delcey.formulairedeconnexion.R;
-
-public class    MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     EditText username, password;
     Button login;
     String userVar, passVar;
+    ProgressDialog progressDialog;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         username = findViewById(R.id.usernameEditText);
         password = findViewById(R.id.passwordEditText);
         login = findViewById(R.id.loginButton);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                userVar = username.getText().toString();
-                passVar = password.getText().toString();
-                if (userVar.equals("")) {
-                    Toast.makeText(getApplicationContext(), "L'identifiant ne peut pas être vide", Toast.LENGTH_SHORT).show();
-                } else if (passVar.equals("")) {
-                    Toast.makeText(getApplicationContext(), "Le mot de passe ne peut pas être vide", Toast.LENGTH_SHORT).show();
-                } else {
-                    //Toast.makeText(getApplicationContext(), "login method to proceed", Toast.LENGTH_SHORT).show();
-                    Login lg = new Login(MainActivity.this);
-                    lg.execute();
-                }
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Connexion en cours... Veuillez patienter");
+        progressDialog.setCancelable(false);
+
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        login.setOnClickListener(view -> {
+            userVar = username.getText().toString();
+            passVar = password.getText().toString();
+            if (userVar.equals("")) {
+                Toast.makeText(getApplicationContext(), "Votre mail n'est pas renseigné", Toast.LENGTH_SHORT).show();
+            } else if (passVar.equals("")) {
+                Toast.makeText(getApplicationContext(), "Votre mot de passe n'est pas renseigné", Toast.LENGTH_SHORT).show();
+            } else {
+                progressDialog.show();
+                loginRequest();
             }
         });
+
     }
 
 
-    class Login extends AsyncTask<String, Void, Integer> {
+    private void loginRequest() {
+        String loginUrl = "https://hugo-rivaux.fr/API/login.php";
 
-        AlertDialog alertDialog;
-        Context context;
-        ProgressDialog progressDialog;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, loginUrl,
+                response -> {
+                    progressDialog.dismiss();
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        int status = jsonObject.getInt("status");
+                        if (status == HttpURLConnection.HTTP_OK) {
+                            username.setText("");
+                            password.setText("");
+                            Intent intentAccueil = new Intent(MainActivity.this, AccueilActivity.class);
 
-        Login(Context ctx) {
-            this.context = ctx;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            progressDialog = ProgressDialog.show(context, "", "Connexion en cours... Veuillez patienter");
-        }
-
-        @Override
-        protected Integer doInBackground(String... params) {
-            String login_url = "https://hugo-rivaux.fr/API/login.php";
-            try {
-                URL url = new URL(login_url);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setConnectTimeout(10000);
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.setDoInput(true);
-                OutputStream os = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-                String data = URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(userVar, "UTF-8") + "&" +
-                        URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(passVar, "UTF-8");
-                bufferedWriter.write(data);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                os.close();
-
-                int responseCode = httpURLConnection.getResponseCode();
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        response.append(line);
+                            startActivity(intentAccueil);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Email ou mot de passe inccorect ", Toast.LENGTH_LONG).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "Échec de l'analyse de la réponse du serveur", Toast.LENGTH_LONG).show();
                     }
-                    bufferedReader.close();
-                    inputStream.close();
-
-                    JSONObject jsonObject = new JSONObject(response.toString());
-                    return jsonObject.getInt("status");
-                } else {
-                    return HttpURLConnection.HTTP_BAD_REQUEST;
+                },
+                error -> {
+                    progressDialog.dismiss();
+                    error.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Échec de connexion au serveur. S'il vous plaît, vérifiez votre connexion à internet et réessayez.", Toast.LENGTH_LONG).show();
                 }
-
-            } catch (IOException | JSONException e) {
-                e.printStackTrace();
-                return HttpURLConnection.HTTP_BAD_REQUEST;
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", userVar);
+                params.put("password", passVar);
+                return params;
             }
-        }
+        };
 
-        @Override
-        protected void onPostExecute(Integer status) {
-            progressDialog.dismiss();
-
-            if (status == HttpURLConnection.HTTP_OK) {
-                username.setText("");
-                password.setText("");
-                Intent intentAccueil = new Intent(MainActivity.this, AccueilActivity.class);
-
-                startActivity(intentAccueil);
-            } else if (status == HttpURLConnection.HTTP_BAD_REQUEST) {
-                Toast.makeText(context, "Email ou mot de passe incorrect ", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(context, "Échec de connexion au serveur. S'il vous plaît, vérifiez votre connexion à internet et réessayez.", Toast.LENGTH_LONG).show();
-            }
-        }
+        progressDialog.show();
+        requestQueue.add(stringRequest);
     }
+
 }
